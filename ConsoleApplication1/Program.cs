84142dcd527bebffe14c06113386c4b7a20d41bc
unknown <zdravohorvat30@mail.ru>
2018-03-13 08:56:36 +0300

    two commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Library
 {
-    class Program
+    class Library
     {
-        static void Main(string[] args)
+
+        public Library()
         {
+            this.Name = "";
+            this.Address = "";
+            this.Open = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("Input name of library: ");
+            this.Name = Console.ReadLine();
+            Console.Write("Input address of library: ");
+            this.Address = Console.ReadLine();
+            Console.Write("Input open of library: ");
+            this.Open = (int)Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Name: { this.Name }");
+            Console.WriteLine("Address: { this.Address }");
+            Console.WriteLine("Open: { this.Open }");
+        }
         }
     }
 }
